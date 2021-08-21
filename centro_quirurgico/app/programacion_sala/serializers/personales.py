bf0f5f704df88bd6ee517a35de59730bd8984030
yur <adanyur@gmail.com>
2021-08-21 12:58:45 -0500

    se mejoro views de personales

--- centro_quirurgico/app/programacion_sala/serializers/personales.py
+++ centro_quirurgico/app/programacion_sala/serializers/personales.py
@@ -1,28 +1,9 @@
 from rest_framework import serializers
 from ..models.personales import *
 
-# class PersonalSerializer(serializers.ModelSerializer):
-#     codigo = serializers.CharField(source='pl_codper')
-#     nombre = serializers.CharField(source='nombre_personal')
-#     tipo = serializers.CharField(default='A')
-
-#     class Meta:
-#         model = Personales
-#         fields = ['codigo','nombre','tipo']
-
-
-# class MedicosSerializers(serializers.ModelSerializer):
-#     codigo = serializers.CharField(source='me_codigo')
-#     nombre = serializers.CharField(source='me_nombres')
-#     tipo   = serializers.CharField()
-#     class Meta:
-#         model = Medicos
-#         fields = ['codigo','nombre','tipo']
-#         read_only_fields = fields
-
-
-
 class PersonalSerializer(serializers.Serializer):
        codigo = serializers.CharField()
        nombre = serializers.CharField()
        tipo   = serializers.CharField()
+       class Meta:
+            read_only_fields = serializers
--- centro_quirurgico/app/programacion_sala/views/personales.py
+++ centro_quirurgico/app/programacion_sala/views/personales.py
@@ -10,16 +10,11 @@ from ..serializers.personales import *
 @api_view(['GET'])
 def personales_api_view(request):
     if request.method == 'GET':
-
         personales = Personales.objects.values_list('pl_codper','pl_nombre').annotate(tipo=Value('A',output_field=CharField())).filter(pl_estado='1')
         medicos = Medicos.objects.values_list('me_codigo','me_nombres').annotate(tipo=Value('M',output_field=CharField())).filter(me_estado='A')
-        # medicos = Medicos.objects.annotate(tipo=Value('M',output_field=CharField())).filter(me_estado='A').values_list('me_codigo','me_nombres','tipo')
-
-        data = personales.union(medicos)
-        for x in data:            
-            personal_serializer = PersonalSerializer(x,many=True)
-            print(personal_serializer)
-        # personal_serializer = PersonalSerializer(data,many=True) 
-        # return Response(personal_serializer.data)
-        return Response({'message':'test'})
-
+        key=('codigo','nombre','tipo')
+        data=[]
+        for value in personales.union(medicos):
+            data.append(dict(zip(key,value)))
+        data_serializer = PersonalSerializer(data,many=True)    
+        return Response(data_serializer.data)
