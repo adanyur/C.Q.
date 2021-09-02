217f995d24c7b17b6d8b3e09c80efdca806b0b78
yur <adanyur@gmail.com>
2021-09-02 16:48:05 -0500

    cambio de fecha de disponibilidad de salas

--- centro_quirurgico/app/programacion_sala/views/disponibilidad_salas.py
+++ centro_quirurgico/app/programacion_sala/views/disponibilidad_salas.py
@@ -9,7 +9,7 @@ from ..serializers.disponibilidad_salas import *
 @api_view(['GET'])
 def disponibilidad_salas_api_view(request):
     if request.method =='GET':
-        disponibilidad_sala = DisponibilidadSalas.objects.raw("SELECT * FROM cq_c_disponibilidad_salas('2021-01-05','02')")
+        disponibilidad_sala = DisponibilidadSalas.objects.raw("SELECT * FROM cq_c_disponibilidad_salas('2021-01-05','01')")
         disponibilidad_serializers = DisponibilidadSalasSerializer(disponibilidad_sala,many=True)
         return Response(disponibilidad_serializers.data)
         
