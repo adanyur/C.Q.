3e97da2d705e50cc45b4b897512902c8b1c01045
yur <adanyur@gmail.com>
2021-09-06 17:16:14 -0500

    se agrego campos para el modelo de agenda SOAP

--- centro_quirurgico/app/agenda_soap/serializers.py
+++ centro_quirurgico/app/agenda_soap/serializers.py
@@ -4,5 +4,22 @@ from .models import ProgramacionSOAP
 class ProgramacionSOAPSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProgramacionSOAP
-        fields = '__all__'
+        fields = ('diahora',
+                'sala',
+                'paciente',
+                'intervencion',
+                'cirujano',
+                'anestesia',
+                'cama',
+                'pedido',
+                'cq_numope',
+                'cq_fecha',
+                'cq_hoinpr',
+                'cq_hofipr',
+                'cq_hoinej',
+                'cq_hofiej',
+                'cq_numhis',
+                'cq_estado',
+                'edad',
+                'semanas')
         read_only_fields = fields
